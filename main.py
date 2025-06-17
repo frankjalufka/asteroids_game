5eb85fb89094880f387e3c312ed8e20b0498e5fd
Frank Jalufka <fljalufka@gmail.com>
2025-06-17 16:08:12 -0500

    Finished player class and updated groups

--- main.py
+++ main.py
@@ -8,7 +8,14 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     dt = 0
 
     while True:
@@ -16,8 +23,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
+        updatable.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+        
+        for obj in drawable:
+            obj.draw(screen)
+            
         pygame.display.flip()
 
         #Will limit framerate to 60 FPS
