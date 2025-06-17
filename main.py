14a25575d4cd35e16a91f29e85343855845535d4
Frank Jalufka <fljalufka@gmail.com>
2025-06-17 16:20:18 -0500

    Added Asteroids, asteroid field, and collision mechanics

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import sys
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 
@@ -11,6 +14,11 @@ def main():
     
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
 
     Player.containers = (updatable, drawable)
 
@@ -25,11 +33,16 @@ def main():
             
         updatable.update(dt)
 
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+
         screen.fill("black")
         
         for obj in drawable:
             obj.draw(screen)
-            
+
         pygame.display.flip()
 
         #Will limit framerate to 60 FPS
